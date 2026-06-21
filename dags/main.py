77742a65262d4be8e0d00f73d2acb7ee31cc5a3f
JoseE-007 <jefengineer@gmail.com>
2026-06-21 12:24:41 -0600

    Dag's re-structure

--- dags/main.py
+++ dags/main.py
@@ -1,7 +1,13 @@
 from airflow import DAG
 import pendulum 
 from datetime import datetime, timedelta
-from api.video_stats import get_playlist_id, get_videoids, extract_video_data, save_to_json
+from airflow.operators.trigger_dagrun import TriggerDagRunOperator
+from api.video_stats import (
+    get_playlist_id, 
+    get_videoids, 
+    extract_video_data, 
+    save_to_json,
+)
 
 from datawarehouse.dwh import staging_table, core_table
 from dataquality.soda import yt_elt_data_quality
@@ -36,7 +42,7 @@ with DAG(
     description="DAG to produce JSON file with raw data", 
     schedule="0 14 * * *", 
     catchup=False,
-) as dag:
+) as dag_produce:
     
     # Define tasks 
     playlist_id = get_playlist_id()
@@ -44,33 +50,41 @@ with DAG(
     extract_data = extract_video_data(video_ids)
     save_to_json_task = save_to_json(extract_data)
 
+    trigger_update_db = TriggerDagRunOperator(
+        task_id="trigger_update_db",
+        trigger_dag_id="update_db",
+    ) 
+
     # Define Dependencies 
-    playlist_id >> video_ids >> extract_data >> save_to_json_task
+    playlist_id >> video_ids >> extract_data >> save_to_json_task >> trigger_update_db
 
     # DAG 2: update_db
 with DAG(
     dag_id="update_db",
     default_args=default_args,
     description="DAG to process JSON file and insert data into both staging and core schemas",
-    schedule="0 15 * * *",
+    schedule=None,
     catchup=False,
-    
-) as dag:
+) as dag_update:
 
     # Define tasks
     update_staging = staging_table()
     update_core = core_table()
 
+    trigger_data_quality = TriggerDagRunOperator(
+        task_id="trigger_data_quality",
+        trigger_dag_id="data_quality",
+    )
 
     # Define dependencies
-    update_staging >> update_core 
+    update_staging >> update_core >> trigger_data_quality
 
 # DAG 3: data_quality
 with DAG(
     dag_id="data_quality",
     default_args=default_args,
     description="DAG to check the data quality on both layers in the database",
-    schedule="0 16 * * *",
+    schedule=None,
     catchup=False,
 ) as dag_quality:
     
